37666af10965174175f4661be9609ca2ce65120a
Erika Sankari <8eikochan@gmail.com>
2017-10-19 20:20:46 +0300

    Implement initial side menu.

--- src/components/sideMenu/SideMenuStyles.js
+++ src/components/sideMenu/SideMenuStyles.js
@@ -1,3 +1,8 @@
 export const sideMenuStyle = {
-
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
 };
