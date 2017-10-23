c51febdfc101ee9d0c10a210a247e44bac88a1b9
Erika Sankari <8eikochan@gmail.com>
2017-10-23 21:50:28 +0300

    Add PriceBlock component.

--- src/components/pricesScreen/PricesScreen.js
+++ src/components/pricesScreen/PricesScreen.js
@@ -1,25 +1,77 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-    Platform,
-    StyleSheet,
     Text,
     View
 } from 'react-native';
 
+import * as strings from '../../res/strings.json';
 import {PricesScreenStyles as styles} from "./PricesScreenStyles";
 import PriceBlock from './PriceBlock';
 
 export default class PricesScreen extends Component {
+    state = {
+        electricityPrices: [
+                {
+                    time: '8.00 - 20.00',
+                    price: 20.53,
+                    icon: 'wb-sunny',
+                    stateIcon: 'arrow-upward'
+                },
+                {
+                    time: '20.00 - 8.00',
+                    price: 17.73,
+                    icon: 'brightness-3',
+                    stateIcon: 'arrow-downward'
+                },
+                {
+                    time: 'weekend',
+                    price: 17.73,
+                    icon: 'star',
+                    stateIcon: 'arrow-downward'
+                }
+            ],
+        gasPrices: [
+                {
+                    time: '8.00 - 20.00',
+                    price: 53,
+                    icon: 'wb-sunny',
+                    stateIcon: 'arrow-downward'
+                },
+                {
+                    time: '20.00 - 8.00',
+                    price: 50,
+                    icon: 'brightness-3',
+                    stateIcon: 'arrow-upward'
+                },
+                {
+                    time: 'weekend',
+                    price: 50,
+                    icon: 'star',
+                    stateIcon: 'arrow-downward'
+                }
+            ]
+    };
+
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.title}>
-                    Electricity prices (Cent/kWh)
+                    {strings.electricityPrices}
                 </Text>
+                {
+                    this.state.electricityPrices.map((item, index) => {
+                        return <PriceBlock item={item} key={index}/>
+                    })
+                }
 
                 <Text style={styles.title}>
-                    Gas prices (Cent/Liter)
+                    {strings.gasPrices}
                 </Text>
+                {
+                    this.state.gasPrices.map((item, index) =>
+                        <PriceBlock item={item} key={index} />
+                    )
+                }
             </View>
         );
     }
